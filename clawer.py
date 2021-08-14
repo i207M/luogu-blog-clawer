7ef3057be06439df303949a6e80e3cbc401ec25f
i207M <connect@mail.i207m.top>
2021-08-14 20:43:08 +0800

    done

--- clawer.py
+++ clawer.py
@@ -5,19 +5,28 @@ import requests
 from headers import headers
 
 ADMIN_BASE = 'https://www.luogu.com.cn/blogAdmin/article/list?status=%5B1%2C2%5D&solution=%5B%5D&page={}&pageType=list&name=&status-check=1&status-check=2&order=0'
-RE_STR = r'/blogAdmin/article/edit/\d+'
 
 # ----------------MODIFY----------------
 ADMIN_PAGE_MAX = 40
 # ----------------MODIFY----------------
 
 
-def process():
-    pass
+def process(url: str):
+    ret = requests.get(url, headers=headers)
+    title = ''
+    for line in ret.text.splitlines():
+        if line.startswith(
+            '<input name="title" class="mdui-textfield-input" type="text" placeholder="标题" value="'
+        ):
+            title = line[85:-3]
+        if line.startswith('        var articleContent = "'):
+            post = line[30:-2]
+            open(title + '.md', 'w').write(post)
 
 
 for admin_page in range(1, ADMIN_PAGE_MAX + 1):
+    print(f'Admin Page #{admin_page}')
     ret = requests.get(ADMIN_BASE, headers=headers)
-    url_list = re.findall(RE_STR, ret.text)
+    url_list = re.findall(r'/blogAdmin/article/edit/\d+', ret.text)
     for url in url_list:
         process('https://www.luogu.com.cn' + url)
