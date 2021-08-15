0addcceec71bc26168f883d57c18e25cd92332da
i207M <connect@mail.i207m.top>
2021-08-15 17:12:14 +0800

    fix encoding

--- clawer.py
+++ clawer.py
@@ -13,6 +13,8 @@ INTERVAL = 10
 
 
 def process(url: str):
+    url = 'https://www.luogu.com.cn' + url
+
     ret = requests.get(url, headers=headers)
     ret = ret.content.decode('utf-8')
     filename = ''
@@ -24,7 +26,7 @@ def process(url: str):
             filename = html.unescape(title)
             filename = re.sub(r'[^\w_.)( -]', '_', filename)
             if title != filename:
-                open('swap.txt', 'a+').write(f'{title} => {filename}\n')
+                open('swap.txt', 'a+', encoding='utf=8').write(f'{title} => {filename}\n')
         if line.startswith('        var articleContent = "'):
             post = line[30:-2]
             post = post.replace(r'\/', '/')
@@ -35,6 +37,10 @@ def process(url: str):
         raise Exception('TitleNotFound')
     print(f'Clawed {filename}')
 
+
+# if __name__ == '__main__':
+#     process('/blogAdmin/article/edit/142252')
+#     exit(0)
 
 if __name__ == '__main__':
     history_set = set(open('history.txt').read().splitlines()
@@ -52,7 +58,7 @@ if __name__ == '__main__':
                 continue
             time.sleep(INTERVAL)
             try:
-                process('https://www.luogu.com.cn' + url)
+                process(url)
             except Exception as e:
                 print(f'WRONG: {url}')
                 print(e)
